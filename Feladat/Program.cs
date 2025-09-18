b9c0908a7efee2b3aa6b07e687881891723ef9e4
Tailor Cornedge <szabok2@kkszki.hu>
2025-09-18 08:44:31 +0200

    Feladat2 is ready

--- Feladat/Program.cs
+++ Feladat/Program.cs
@@ -10,6 +10,32 @@ namespace Feladat
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adjon meg egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+            if (szam % 2 == 0) {
+                Console.WriteLine("Ez a szám páros");
+            }
+            else
+            {
+                Console.WriteLine("Ez a szám páratan.");
+            }
+            if (szam > 0)
+            {
+                Console.WriteLine("A szám pozitív");
+            }
+            else if (szam < 0) {
+                Console.WriteLine("A szám negatív");
+            }
+            else
+            {
+                Console.WriteLine("A szám nulla");
+            }
+            Console.WriteLine("Adjon meg egy hónapszámot (1-12): ");
+            int honap = int.Parse(Console.ReadLine());
+            if (0<=honap<=1)
+            {
+
+            }
         }
     }
 }
